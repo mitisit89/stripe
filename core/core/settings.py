4c7c07a1a59c54f959bd593e345dd6f76c1d7d4a
mitisit89 <mitisit89@gmail.com>
2022-11-21 18:53:31 +0300

    fix

--- core/core/settings.py
+++ core/core/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = "django-insecure-z3$&8pjy-y&)h$b2rmp(_^nt)8+)_p55%b1*fhn9h0$n74$*y_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 STRIPE_SECRET = os.getenv("STRIPE_SECRET")
